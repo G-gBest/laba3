73193024f61c055f4924287caaf6da3b080c0f3e
G-gBest <vladyslav.ivakhno@nure.ua>
2021-12-08 22:21:45 +0200

    hnjjh

--- L3.js
+++ L3.js
@@ -1,56 +1,48 @@
-let min = 5;// минимуим студентов в группе
-let max = 30;// максимум студентов в группе
-let five = 0;// количество студентов n-ных груп
-let four = 0;
-let three = 0;
-let two = 0;
-//класс студент
+let min = 5, max = 30, five = 0, four = 0, three = 0, two = 0;
 class student {
     constructor(names) {
         this.name = names[generateNumber(0,listNames.length -1)];
         this.progress = 5;
         this.marks = [];
         for (let i = 0; i < 5; i++) {
-            this.marks.push(generateNumber(2,6));
+            this.marks.push(generateNumber(2,5));
             if(this.marks[i] < this.progress ) {
                 this.progress = this.marks[i];
             }
         }
     }
 }
-// функция генерации случайного числа в диапозоне от min до max
 function generateNumber(min,max){
     return (Math.random() * (max - min) + min).toFixed(0);
 }
-//функция создания и добавления студента в массив студентов
 function createStudent(students,min, max){
     let stdAmount = generateNumber(min,max);
     for (let i = 0; i < stdAmount; i++) {
         let newStudent = new student(listNames);
         students.push(newStudent);
     }
-
 }
-// Список случайных фамилий
-let listNames = ["Иванов","Петров","Сидоров","Кофанов","Арсточцкий","Сергеев","Алекеев","Безбрежный","Дьяченко","Скоробагатько","Белов","Голобородько","Ивасюк","Кирилов","Данилов","Демидов","Морозов","Тимуров"];
-let listStudents = [];// массив студентов
-//функция генерации списка успеваемости
+let listNames = ["Иванов","Петров","Сидоров","Островский","Пушкин","Шевченко","Алекеев","Васильченко","Лещенко","Базарный","Белов","Чубач","Герасименко","Макаров","Щербаков","абрамов","Морозов","Тимуров"];
+let listStudents = [];
 function generateList(students){
-    let html = ""; // html списка студентов
+    let html = ""; 
     createStudent(students,min,max);
-    students.sort(function (a, b){ // сортировка студентов по убыванию
+    students.sort(function (a, b){ 
         return b.progress - a.progress;
     });
     for (let i = 0; i < students.length; i++) {
-        html+="<p>" + students[i].name + " - ";
-            switch (+students[i].progress) {// определение группы студента
-            case 5: html+= "отличник";
+        html+="<p>" + students[i].name+ " ";
+        for (let j = 0; j < 5; j++) {
+            html+= students[i].marks[j] + "."
+        };
+            switch (+students[i].progress) {
+            case 5: 
                 ++five; break;
-            case 4: html+= "хорошист";
+            case 4: 
                 ++four; break;
-            case 3: html+= "средний";
+            case 3: 
                 ++three; break;
-            case 2: html+= "неуспевающий";
+            case 2: 
                 ++two; break;
                 default: alert( 'd' ); break;
         }
